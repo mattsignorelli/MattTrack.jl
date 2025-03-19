f8260c14316fc74902a3c8018bd8c04cbbc5872f
Documenter.jl <documenter@juliadocs.github.io>
2025-03-19 19:49:54 +0000

    build based on c7fe349

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MattTrack","category":"page"},{"location":"#MattTrack","page":"Home","title":"MattTrack","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MattTrack.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MattTrack]","category":"page"}]
+}
